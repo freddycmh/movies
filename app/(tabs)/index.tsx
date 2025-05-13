9bd03f398823adf035d9e2f62463004ec3394c5a
freddycmh <fleddymok@gmail.com>
2025-05-13 15:23:38 -0400

    added assets to the project

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-5xl text-dark-200 font-bold">weekoffee</Text>
+      <Text className="text-5xl text-dark-200 font-bold">Hello World</Text>
 
 
 
